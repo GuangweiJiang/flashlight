b85a4a937459e1b0430e04a62990742e5e49e034
Guangwei Jiang/WKS/Wistron <Guangewi_Jiang@wistron.com>
2014-01-09 14:34:55 +0800

    v1.2.0 Add "shake" feature and version control label; 1. Add new feature which allow user to "shake" to control flash on/off; 2. Add the version label;

--- gen/com/guangwei/flashlight/R.java
+++ gen/com/guangwei/flashlight/R.java
@@ -10,6 +10,9 @@ package com.guangwei.flashlight;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int red=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,26 +27,33 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int checkBoxEnableFlashInit=0x7f080002;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080001;
+        public static final int action_settings=0x7f090006;
+        public static final int checkBoxEnableFlashInit=0x7f090002;
+        public static final int checkBoxEnableShake=0x7f090003;
+        public static final int textView1=0x7f090000;
+        public static final int textView2=0x7f090001;
+        public static final int textView3=0x7f090004;
+        public static final int textView4=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int app_ver=0x7f050007;
         public static final int enable_flashlight=0x7f050006;
+        public static final int enable_shake_trim=0x7f050008;
         public static final int flash_name=0x7f050003;
         public static final int flash_off=0x7f050005;
+        public static final int flash_off_shake=0x7f05000b;
         public static final int flash_open=0x7f050004;
+        public static final int flash_open_shake=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int ver_string=0x7f050009;
     }
     public static final class style {
         /** 
@@ -67,10 +77,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- src/com/guangwei/flashlight/MainActivity.java
+++ src/com/guangwei/flashlight/MainActivity.java
@@ -14,6 +14,10 @@
  * 		
  * 
  * Notes:
+ * 		v1.2.0, by Guangwei.Jiang@Jan09'14
+ * 		1. Add new feature which allow user to "shake" to control flash on/off;
+ * 		2. Add the version label;
+ * 
  * 		v1.1.0, by Guangwei.Jiang@Jan08'14
  * 		1. Add new feature which allow user to choose if open flash light by default;
  */
@@ -24,8 +28,14 @@ import java.util.List;
 
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -41,13 +51,171 @@ public class MainActivity extends Activity {
 	private static final String TAG = "FlashLight";
 	public static final String PREF = "flashlight_pref";
 	public static final String PREF_isEnableFlashInit = "isEnableFlashInit";
+	public static final String PREF_isEnableShake = "isEnableShake";
 	private Camera camera = null;
 	private boolean bFlashState = false;
 	private boolean bEnableFlashInit = false;
+	private boolean bEnableShake = false;
 	
 	private TextView textFlashState = null;
 	private CheckBox checkboxEnableFlashInit = null;
+	private CheckBox checkboxEnableShake = null;
 	
+	//检测摇动相关变量
+    private long lastTime = 0;
+    private long curTime = 0;
+    // 控制时间间隔
+    private int TimeInterval = 400;
+    // 重力加速度阀值
+    private int gravityThreshold = 14;
+    
+    private SensorManager mSensorManager;      
+    private Vibrator mVibrator;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        camera = Camera.open();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+                
+        textFlashState = (TextView) findViewById(R.id.textView2);
+        checkboxEnableFlashInit = (CheckBox) findViewById(R.id.checkBoxEnableFlashInit);
+        checkboxEnableShake = (CheckBox) findViewById(R.id.checkBoxEnableShake);
+        
+        restorePrefs();   
+        
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager == null) {  
+            throw new UnsupportedOperationException();  
+        }  
+        Sensor sensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); 
+        if (sensor == null) {  
+            throw new UnsupportedOperationException();  
+        }  
+        boolean success = mSensorManager.registerListener(mySensorEventListener, sensor,SensorManager.SENSOR_DELAY_GAME);
+        if (!success) {  
+            throw new UnsupportedOperationException();  
+        }else{
+        	System.out.println("Register Listener sucess");
+        }
+        
+        mVibrator = (Vibrator) getApplication().getSystemService(Context.VIBRATOR_SERVICE);
+                        
+        if (bFlashState) {
+        	turnLightOn(camera);
+        	if (bEnableShake) {
+        		textFlashState.setText(R.string.flash_off_shake);
+        	} else {
+        		textFlashState.setText(R.string.flash_off);
+        	}        	
+        }
+        else {
+        	turnLightOff(camera);
+        	if (bEnableShake) {
+        		textFlashState.setText(R.string.flash_open_shake);
+        	} else {
+        		textFlashState.setText(R.string.flash_open);
+        	} 
+        }
+        
+        textFlashState.setOnClickListener(new TextView.OnClickListener()
+        {
+			@Override
+			public void onClick(View arg0) {
+				if (!bEnableShake) {
+					if (!bFlashState) {
+						bFlashState = true;
+						turnLightOn(camera);
+						textFlashState.setText(R.string.flash_off);
+					} else {
+						bFlashState = false;
+						turnLightOff(camera);
+						textFlashState.setText(R.string.flash_open);
+					}
+				}
+			}        	
+        });
+        
+        checkboxEnableFlashInit.setOnCheckedChangeListener
+        (new CheckBox.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+				SharedPreferences settings = getSharedPreferences(PREF, 0);
+				
+				if (checkboxEnableFlashInit.isChecked()) {
+					settings.edit().putBoolean(PREF_isEnableFlashInit, true).commit();
+				} else {
+					settings.edit().putBoolean(PREF_isEnableFlashInit, false).commit();
+				}					
+			}
+        	
+        });        
+        
+        checkboxEnableShake.setOnCheckedChangeListener
+        (new CheckBox.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+				SharedPreferences settings = getSharedPreferences(PREF, 0);
+				
+				if (checkboxEnableShake.isChecked()) {
+					bEnableShake = true;
+				} else {
+					bEnableShake = false;
+				}	
+				settings.edit().putBoolean(PREF_isEnableShake, bEnableShake).commit();
+				
+				if (bEnableShake) {
+					if (bFlashState) {
+						textFlashState.setText(R.string.flash_off_shake);
+					} else {
+						textFlashState.setText(R.string.flash_open_shake);
+					}
+				} else {
+					if (bFlashState) {
+						textFlashState.setText(R.string.flash_off);
+					} else {
+						textFlashState.setText(R.string.flash_open);
+					}
+				}
+			}        	
+        });        
+    }        
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    @Override  
+    protected void onResume() {  
+        super.onResume(); 
+    }
+    
+    @Override  
+    protected void onPause() {  
+        super.onPause(); 
+    }
+    
+    @Override 
+    public boolean onKeyDown(int keyCode,KeyEvent event) {  
+    	// check if it's "back" button  
+    	if (keyCode == KeyEvent.KEYCODE_BACK) {  
+    		super.onDestroy();
+        	turnLightOff(camera);
+        	if (mSensorManager != null) {
+        		mSensorManager.unregisterListener(mySensorEventListener);
+        	}
+        	// Terminate the current process  
+            android.os.Process.killProcess(android.os.Process.myPid());  
+        	return true;
+    	} else {  
+    		return super.onKeyDown(keyCode, event);  
+    	}
+    }
+    
 	public static void turnLightOn(Camera mCamera) {
 		try{
 			if (mCamera == null) {
@@ -101,97 +269,52 @@ public class MainActivity extends Activity {
 			}
 		} catch(Exception ex){}
 	}
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        camera = Camera.open();
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-                
-        textFlashState = (TextView) findViewById(R.id.textView2);
-        checkboxEnableFlashInit = (CheckBox) findViewById(R.id.checkBoxEnableFlashInit);
-        
-        restorePrefs();     
-                
-        if (bFlashState)
-        {
-        	turnLightOn(camera);
-        	textFlashState.setText(R.string.flash_off);
-        }
-        else
-        {
-        	turnLightOff(camera);
-        	textFlashState.setText(R.string.flash_open);
-        }
-        
-        textFlashState.setOnClickListener(new TextView.OnClickListener()
-        {
-
-			@Override
-			public void onClick(View arg0) {
-				// TODO Auto-generated method stub
-				if (!bFlashState)
-		        {
-					bFlashState = true;
-		        	turnLightOn(camera);
-		        	textFlashState.setText(R.string.flash_off);
-		        }
-		        else
-		        {
-		        	bFlashState = false;
-		        	turnLightOff(camera);
-		        	textFlashState.setText(R.string.flash_open);
-		        }
-			}
-        	
-        }
-        );
-        
-        checkboxEnableFlashInit.setOnCheckedChangeListener
-        (new CheckBox.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
-				SharedPreferences settings = getSharedPreferences(PREF, 0);
-				
-				if (checkboxEnableFlashInit.isChecked()) {
-					settings.edit().putBoolean(PREF_isEnableFlashInit, true).commit();
-				} else {
-					settings.edit().putBoolean(PREF_isEnableFlashInit, false).commit();
-				}					
-			}
-        	
-        });
-        
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-
-    @Override 
-    public boolean onKeyDown(int keyCode,KeyEvent event) {  
-    	// check if it's "back" button  
-    	if (keyCode == KeyEvent.KEYCODE_BACK) {  
-    		super.onDestroy();
-        	turnLightOff(camera);
-        	// Terminate the current process  
-            android.os.Process.killProcess(android.os.Process.myPid());  
-        	return true;
-    	} else {  
-    		return super.onKeyDown(keyCode, event);  
-    	}
-    }
     
     // Restore preferences
     private void restorePrefs() {
     	SharedPreferences settings = getSharedPreferences(PREF, 0);
     	bEnableFlashInit = settings.getBoolean(PREF_isEnableFlashInit, false);
+    	bEnableShake = settings.getBoolean(PREF_isEnableShake, false);
     	bFlashState = bEnableFlashInit;
     	checkboxEnableFlashInit.setChecked(bEnableFlashInit);
+    	checkboxEnableShake.setChecked(bEnableShake);
     }
+    
+    public final SensorEventListener mySensorEventListener = new SensorEventListener() {
+
+		@Override
+		public void onAccuracyChanged(Sensor arg0, int arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onSensorChanged(SensorEvent event) {
+			//获取加速度传感器的三个参数
+			float x = event.values[0];
+			float y = event.values[1];
+			float z = event.values[2];
+			//获取当前时刻的毫秒数
+			curTime = System.currentTimeMillis();
+			if(bEnableShake){
+				if ((curTime - lastTime) > TimeInterval) {
+					if ((Math.abs(x) > gravityThreshold) || (Math.abs(y) > gravityThreshold)
+							|| (Math.abs(z) > gravityThreshold)) {
+						//此处开始执行 
+						mVibrator.vibrate( new long[]{100, 10, 100, 500}, -1);
+						if (!bFlashState) {
+							bFlashState = true;
+							turnLightOn(camera);
+							textFlashState.setText(R.string.flash_off_shake);
+						} else {
+							bFlashState = false;
+							turnLightOff(camera);
+							textFlashState.setText(R.string.flash_open_shake);
+						}
+					}
+					lastTime = curTime;
+				}
+			}
+		}    	
+    };
 }
